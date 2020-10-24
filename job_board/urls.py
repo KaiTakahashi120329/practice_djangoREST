8c3d3e71a53b81330f7c59fbe07d9ef5e296d2b3
KaiTakahashi <takahashi.329aaa@gmail.com>
2020-10-24 16:03:59 +0900

    誤字修正

--- job_board/urls.py
+++ job_board/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('spi/', include('jobs.api.urls'))
+    path('api/', include('jobs.api.urls'))
 ]
