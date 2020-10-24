df7a64550dc0830e86af98488ca2078b6841f7d8
KaiTakahashi <takahashi.329aaa@gmail.com>
2020-10-24 22:29:53 +0900

    router 追加

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,45 +1,14 @@
-<<<<<<< HEAD
-import Vue from "vue";
-import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
-=======
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '../views/Home.vue';
->>>>>>> test
 
 Vue.use(VueRouter);
 
 const routes = [
-<<<<<<< HEAD
-  {
-    path: "/",
-    name: "Home",
-    component: Home
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
-];
-
-const router = new VueRouter({
-  mode: "history",
-  base: process.env.BASE_URL,
-  routes
-});
-
-export default router;
-=======
     {
         path: "/",
         name: "Home",
-        component: Hoem
+        component: Home
     },
 ];
 
@@ -49,4 +18,3 @@ const router = new VueRouter({
 });
 
 export default router;
->>>>>>> test
